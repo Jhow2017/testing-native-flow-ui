c6e5787cda2c1f951e01e72ef8f9f231072b4740
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-23 16:20:04 -0400

    add validate as is 'View' | 'Text' | 'TouchableOpacity' | 'ScrollView'

--- src/designer-system/core/component-mounter/index.tsx
+++ src/designer-system/core/component-mounter/index.tsx
@@ -7,18 +7,19 @@ import { BaseViewTypesProps } from '../types/view';
 // hooks
 import { BreakpointValue } from '../types/breakpointValue';
 import { createStyleSheet, useStyles } from '@ds/config/unistyles';
+import { StyledComponents } from '../styles';
 
 // styles
-import { BaseStyleView } from '../styles';
 
 export interface ComponentMounterType extends BaseViewTypesProps {
     children: ReactNode;
     _platform?: (platform: typeof Platform) => ViewStyle;
     _css?: BreakpointValue<string>;
+    as?: keyof typeof StyledComponents;
 }
 
 const ComponentMounter: React.FC<ComponentMounterType> = (props) => {
-    const { children, _platform, _css, ...rest } = props;
+    const { children, _platform, _css, as = 'View', ...rest } = props;
 
     const stylesheet = createStyleSheet(() => ({
         flexStyle: {
@@ -32,13 +33,15 @@ const ComponentMounter: React.FC<ComponentMounterType> = (props) => {
     const additionalStyles: any =
         rest.style instanceof Array ? rest.style : [rest.style];
 
+    const BaseComponent = StyledComponents[as] || StyledComponents.View;
+
     return (
-        <BaseStyleView
+        <BaseComponent
             style={[styles.flexStyle, ...additionalStyles, platformStyles]}
             _css={_css}
         >
             {children}
-        </BaseStyleView>
+        </BaseComponent>
     );
 };
 
--- src/designer-system/core/styles/index.ts
+++ src/designer-system/core/styles/index.ts
@@ -1,4 +1,10 @@
-import styled, { css } from 'styled-components/native';
+import styled from 'styled-components/native';
+import {
+    ViewProps,
+    TextProps,
+    TouchableOpacityProps,
+    ScrollViewProps,
+} from 'react-native';
 
 import generateCustomCss from '../utils/generateCustomCss';
 import { BreakpointValue } from '../types/breakpointValue';
@@ -7,10 +13,27 @@ interface BaseStyleViewProps {
     _css?: BreakpointValue<string>;
 }
 
-export const BaseStyleView = styled.View<BaseStyleViewProps>`
-    ${({ _css }) =>
-        _css &&
-        css`
-            ${generateCustomCss(_css)}
-        `}
-`;
+type StyledComponentType = {
+    View: React.ComponentType<ViewProps & BaseStyleViewProps>;
+    Text: React.ComponentType<TextProps & BaseStyleViewProps>;
+    button: React.ComponentType<TouchableOpacityProps & BaseStyleViewProps>;
+    ScrollView: React.ComponentType<ScrollViewProps & BaseStyleViewProps>;
+};
+
+const applyCustomStyles = (styleProps: BaseStyleViewProps) =>
+    styleProps._css ? generateCustomCss(styleProps._css) : '';
+
+export const StyledComponents: StyledComponentType = {
+    View: styled.View`
+        ${applyCustomStyles}
+    `,
+    Text: styled.Text`
+        ${applyCustomStyles}
+    `,
+    button: styled.TouchableOpacity`
+        ${applyCustomStyles}
+    `,
+    ScrollView: styled.ScrollView`
+        ${applyCustomStyles}
+    `,
+};
