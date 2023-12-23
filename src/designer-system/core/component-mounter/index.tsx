35fe5efdd38f54b907c134af844ff3196ba67265
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-23 02:35:19 -0400

    refactor ComponentMounter

--- src/designer-system/core/component-mounter/index.tsx
+++ src/designer-system/core/component-mounter/index.tsx
@@ -1,24 +1,38 @@
 import React, { ReactNode } from 'react';
-import { View } from 'react-native';
+import { Platform, ViewStyle } from 'react-native';
 
 // types
 import { BaseViewTypesProps } from '../types/view';
 
 // hooks
 import useResponsiveStyles from '@ds/hooks/useResponsiveStyles';
+import { BreakpointValue } from '../types/breakpointValue';
+
+// styles
+import { BaseStyleView } from '../styles';
 
 export interface ComponentMounterType extends BaseViewTypesProps {
     children: ReactNode;
+    _platform?: (platform: typeof Platform) => ViewStyle;
+    _css?: BreakpointValue<string>;
 }
 
 const ComponentMounter: React.FC<ComponentMounterType> = (props) => {
-    const { children, ...rest } = props;
+    const { children, _platform, _css, ...rest } = props;
     const style = useResponsiveStyles(rest);
 
+    const platformStyles = _platform ? _platform(Platform) : {};
     const additionalStyles: any =
         rest.style instanceof Array ? rest.style : [rest.style];
 
-    return <View style={[style, ...additionalStyles]}>{children}</View>;
+    return (
+        <BaseStyleView
+            style={[style, ...additionalStyles, platformStyles]}
+            _css={_css}
+        >
+            {children}
+        </BaseStyleView>
+    );
 };
 
 export default ComponentMounter;
