3e2f0207ebef05c97ebc0ef3c2a1f36660cd3bf3
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-23 00:45:39 -0400

    create hooks debounce and useResponsiveStyles

---
+++ src/designer-system/hooks/debounce.ts
@@ -0,0 +1,31 @@
+export function debounce<F extends (...args: any[]) => void>(
+    func: F,
+    wait: number,
+    immediate: boolean = false
+): ((...args: Parameters<F>) => void) & { cancel: () => void } {
+    let timeout: NodeJS.Timeout | null = null;
+
+    const debouncedFunction = (...args: Parameters<F>) => {
+        const later = () => {
+            timeout = null;
+            if (!immediate) func(...args);
+        };
+
+        const callNow = immediate && !timeout;
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func(...args);
+    };
+
+    debouncedFunction.cancel = () => {
+        if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+        }
+    };
+
+    return debouncedFunction;
+}
