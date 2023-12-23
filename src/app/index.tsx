af602bfd19f00f9856005a46f0813d042bb12d04
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-23 15:06:31 -0400

    add provider theme in App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,13 @@
-import { DsText } from '@ds/components/typography';
-import { DsFlex, DsBox } from '@ds/layout';
+import { UnistylesTheme } from 'react-native-unistyles';
+import { theme } from '@ds/config/theme';
 
-export default function TabOneScreen() {
+// component
+import Home from '@components/home';
+
+export default function App() {
     return (
-        <DsBox
-            flex={1}
-            justifyContent={{ sm: 'center' }}
-            alignItems={{ sm: 'center', md: 'center' }}
-            //backgroundColor={{ sm: 'blue', md: 'blue' }}
-            gap={{ sm: 24, md: 24 }}
-            // _platform={(e) => ({
-            //     backgroundColor: e.OS === 'ios' ? 'red' : 'blue',
-            //     marginTop: e.OS === 'ios' ? 0 : 200,
-            // })}
-            // _css={'background-color: red;'}
-            _css={{
-                sm: 'background-color: blue;',
-                md: 'background-color: green; ',
-            }}
-        >
-            <DsText color="green" fontSize={24}>
-                Olá!!
-            </DsText>
-            <DsText>Olá!!</DsText>
-        </DsBox>
+        <UnistylesTheme theme={theme}>
+            <Home />
+        </UnistylesTheme>
     );
 }
