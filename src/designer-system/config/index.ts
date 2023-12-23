119853c18e3974ea0d80b7006d0ea8fc56d9e8f2
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-23 15:05:55 -0400

    news config

--- src/designer-system/config/index.ts
+++
@@ -1,1 +0,0 @@
-export const breakpoints = { sm: 0, md: 600, lg: 960 };
---
+++ src/designer-system/config/theme/index.ts
@@ -0,0 +1,5 @@
+import { colors } from './colors';
+import { breakpoints } from './breakpoints';
+import { FontSize } from './fontSize';
+
+export const theme = { colors, breakpoints, FontSize } as const;
