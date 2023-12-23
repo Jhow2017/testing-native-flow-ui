92dd734acfb9c00fad890663e05c17c40c50a289
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-23 02:26:50 -0400

    index app testing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,28 @@
 import { DsText } from '@ds/components/typography';
-import { DsFlex } from '@ds/layout';
-import { Text } from 'react-native';
+import { DsFlex, DsBox } from '@ds/layout';
 
 export default function TabOneScreen() {
     return (
-        <DsFlex
+        <DsBox
             flex={1}
             justifyContent={{ sm: 'center' }}
             alignItems={{ sm: 'center', md: 'center' }}
-            backgroundColor={{ sm: 'red', md: 'blue' }}
+            //backgroundColor={{ sm: 'blue', md: 'blue' }}
             gap={{ sm: 24, md: 24 }}
+            // _platform={(e) => ({
+            //     backgroundColor: e.OS === 'ios' ? 'red' : 'blue',
+            //     marginTop: e.OS === 'ios' ? 0 : 200,
+            // })}
+            // _css={'background-color: red;'}
+            _css={{
+                sm: 'background-color: blue;',
+                md: 'background-color: green; ',
+            }}
         >
             <DsText color="green" fontSize={24}>
                 Olá!!
             </DsText>
             <DsText>Olá!!</DsText>
-        </DsFlex>
+        </DsBox>
     );
 }
