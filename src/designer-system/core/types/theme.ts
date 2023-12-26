832015c087bd6ca4d4ee3c7e12f8819b7cf2c9fd
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-25 20:40:12 -0400

    add type FontSize in BaseThemeTypes

--- src/designer-system/core/types/theme.ts
+++ src/designer-system/core/types/theme.ts
@@ -1,3 +1,4 @@
+import { sizeMapping } from '@ds/components/global/icon/types';
 import { BreakpointValue } from './breakpointValue';
 
 export type Color = 'red' | 'blue' | string;
@@ -9,7 +10,10 @@ export type FontFamily =
     | 'Inter_900Black';
 export type BreakpointKey = 'sm' | 'md' | 'lg';
 
+type FontSize = keyof typeof sizeMapping;
+
 export type BaseThemeTypes = {
     backgroundColor?: BreakpointValue<Color>;
     fontFamily?: BreakpointValue<FontFamily>;
+    fontSize?: BreakpointValue<FontSize> | number;
 };
