7c188b64bef38cb2f22a0df452ca9d8f1ab333be
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2023-12-26 20:42:53 -0400

    add DsModal in Home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -8,109 +8,108 @@ export default function Home() {
     const [isModalVisible, setModalVisible] = useState(false);
 
     return (
-        <>
-            <DsBox
-                flex={1}
-                width={'100%'}
-                justifyContent={{ xs: 'center' }}
-                alignItems={{ xs: 'center', md: 'center' }}
-                backgroundColor={{ xs: 'blue', md: 'blue' }}
+        <DsBox
+            flex={1}
+            width={'100%'}
+            justifyContent={{ xs: 'center' }}
+            alignItems={{ xs: 'center', md: 'center' }}
+            backgroundColor={{ xs: 'blue', md: 'blue' }}
+            // backgroundColor={{
+            //     ':w[200,500]': 'red',
+            //     ':w[500,900]': 'blue',
+            // }}
+            // _platform={(e) => ({
+            //     backgroundColor: e.OS === 'ios' ? 'red' : 'blue',
+            //     marginTop: e.OS === 'ios' ? 150 : 300,
+            // })}
+            // _css={'background-color: red;'}
+            // _css={{
+            //     xs: 'background-color: blue;',
+            //     md: 'background-color: green; ',
+            // }}
+        >
+            <DsIcon
+                icon="close"
+                color="#fff"
+                fontSize={24}
+                onPress={() => setModalVisible(true)}
+                //backgroundColor={'#000'}
+                // marginTop={{
+                //     xs: 100,
+                // }}
+                // style={{
+                //     backgroundColor: 'green',
+                // }}
+                // _platform={(e) => ({
+                //     backgroundColor: e.OS === 'ios' ? 'blue' : 'red',
+                //     marginTop: e.OS === 'ios' ? 150 : 300,
+                // })}
+                flexDirection={{
+                    xs: 'row',
+                }}
+                alignItems={'center'}
+                gap={2}
+                stylesText={{
+                    color: 'red',
+                    fontSize: 18,
+                }}
+            >
+                Abrir o Modal
+            </DsIcon>
+            <DsText
+                color="#fff"
+                textTransform="capitalize"
+                numberOfLines={1}
+                ellipsizeMode="tail"
+                width={'80%'}
+                fontSize={24}
                 // backgroundColor={{
                 //     ':w[200,500]': 'red',
                 //     ':w[500,900]': 'blue',
                 // }}
                 // _platform={(e) => ({
-                //     backgroundColor: e.OS === 'ios' ? 'red' : 'blue',
+                //     backgroundColor: e.OS === 'ios' ? 'green' : 'red',
                 //     marginTop: e.OS === 'ios' ? 150 : 300,
                 // })}
                 // _css={'background-color: red;'}
                 // _css={{
-                //     xs: 'background-color: blue;',
+                //     xs: 'background-color: red;',
                 //     md: 'background-color: green; ',
                 // }}
+                // style={{
+                //     backgroundColor: 'red',
+                // }}
             >
-                <DsIcon
-                    icon="close"
-                    color="#fff"
-                    fontSize={24}
-                    onPress={() => setModalVisible(true)}
-                    //backgroundColor={'#000'}
-                    // marginTop={{
-                    //     xs: 100,
-                    // }}
-                    // style={{
-                    //     backgroundColor: 'green',
-                    // }}
-                    // _platform={(e) => ({
-                    //     backgroundColor: e.OS === 'ios' ? 'blue' : 'red',
-                    //     marginTop: e.OS === 'ios' ? 150 : 300,
-                    // })}
-                    flexDirection={{
-                        xs: 'row',
-                    }}
-                    alignItems={'center'}
-                    gap={2}
-                    stylesText={{
-                        color: 'red',
-                        fontSize: 18,
-                    }}
-                >
-                    Abrir o Modal
-                </DsIcon>
-                <DsText
-                    color="#fff"
-                    textTransform="capitalize"
-                    numberOfLines={1}
-                    ellipsizeMode="tail"
-                    width={'80%'}
-                    fontSize={24}
-                    // backgroundColor={{
-                    //     ':w[200,500]': 'red',
-                    //     ':w[500,900]': 'blue',
-                    // }}
-                    // _platform={(e) => ({
-                    //     backgroundColor: e.OS === 'ios' ? 'green' : 'red',
-                    //     marginTop: e.OS === 'ios' ? 150 : 300,
-                    // })}
-                    // _css={'background-color: red;'}
-                    // _css={{
-                    //     xs: 'background-color: red;',
-                    //     md: 'background-color: green; ',
-                    // }}
-                    // style={{
-                    //     backgroundColor: 'red',
-                    // }}
-                >
-                    texto bem grande texto bem grande texto bem grande texto bem
-                    grande
+                texto bem grande texto bem grande texto bem grande texto bem
+                grande
+            </DsText>
+
+            <DsModal
+                visible={isModalVisible}
+                onClose={() => setModalVisible(false)}
+                backgroundColor={{
+                    xs: '#fff',
+                    md: 'red',
+                }}
+            >
+                <DsText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nunc vitae massa odio. Quisque ante sem, tempor eget massa
+                    vel, mollis tincidunt metus. Ut sed felis lectus. Nam semper
+                    molestie urna, quis ultricies quam semper ut. Maecenas
+                    aliquet id urna a convallis. Class aptent taciti sociosqu ad
+                    litora torquent per conubia nostra, per inceptos himenaeos.
+                    Maecenas leo lectus, dictum vitae erat eget, luctus dapibus
+                    sapien. Integer at hendrerit quam. Vivamus tempor, arcu non
+                    fringilla laoreet, enim nibh porttitor enim, eget
+                    pellentesque eros nulla congue neque. Suspendisse et
+                    lobortis enim, nec fermentum est. Aliquam accumsan viverra
+                    vehicula. Proin tempus sagittis auctor. Vivamus quam ligula,
+                    laoreet eget eros et, hendrerit iaculis risus. Nam a nulla
+                    in purus fermentum rhoncus eu et erat. Aliquam tempus felis
+                    lorem, id hendrerit tortor vestibulum ac.
                 </DsText>
-                {isModalVisible && (
-                    <DsModal
-                        visible={isModalVisible}
-                        onClose={() => setModalVisible(false)}
-                    >
-                        <DsText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Nunc vitae massa odio. Quisque ante sem,
-                            tempor eget massa vel, mollis tincidunt metus. Ut
-                            sed felis lectus. Nam semper molestie urna, quis
-                            ultricies quam semper ut. Maecenas aliquet id urna a
-                            convallis. Class aptent taciti sociosqu ad litora
-                            torquent per conubia nostra, per inceptos himenaeos.
-                            Maecenas leo lectus, dictum vitae erat eget, luctus
-                            dapibus sapien. Integer at hendrerit quam. Vivamus
-                            tempor, arcu non fringilla laoreet, enim nibh
-                            porttitor enim, eget pellentesque eros nulla congue
-                            neque. Suspendisse et lobortis enim, nec fermentum
-                            est. Aliquam accumsan viverra vehicula. Proin tempus
-                            sagittis auctor. Vivamus quam ligula, laoreet eget
-                            eros et, hendrerit iaculis risus. Nam a nulla in
-                            purus fermentum rhoncus eu et erat. Aliquam tempus
-                            felis lorem, id hendrerit tortor vestibulum ac.
-                        </DsText>
-                    </DsModal>
-                )}
-            </DsBox>
-        </>
+            </DsModal>
+        </DsBox>
     );
 }
