4060d0df74bcbad0cc8cd7d4abf9e728f6cbd8c4
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2024-01-01 19:24:08 -0400

    remove FontSizetype in BaseThemeTypes

--- src/designer-system/core/types/theme.ts
+++ src/designer-system/core/types/theme.ts
@@ -1,4 +1,3 @@
-import { sizeMapping } from '@ds/components/global/icon/types';
 import { BreakpointValue } from './breakpointValue';
 
 export type Color = 'red' | 'blue' | string;
@@ -10,10 +9,8 @@ export type FontFamily =
     | 'Inter_900Black';
 export type BreakpointKey = 'sm' | 'md' | 'lg';
 
-type FontSize = keyof typeof sizeMapping;
-
 export type BaseThemeTypes = {
     backgroundColor?: BreakpointValue<Color>;
     fontFamily?: BreakpointValue<FontFamily>;
-    fontSize?: BreakpointValue<FontSize> | number;
+    fontSize?: BreakpointValue<number> | number;
 };
