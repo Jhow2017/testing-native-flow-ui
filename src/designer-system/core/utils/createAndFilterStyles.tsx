8ed15c28aa2ef0980110e7dc57d622770c3ceaee
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2024-01-02 00:46:07 -0400

    create createAndFilterStyles

---
+++ src/designer-system/core/utils/createAndFilterStyles.tsx
@@ -0,0 +1,28 @@
+import { createStyleSheet, useStyles } from '@ds/config/unistyles';
+import { cssValue } from './constants';
+
+interface StyleObject {
+    [key: string]: any;
+}
+
+const createAndFilterStyles = (rest: StyleObject) => {
+    const stylesheet = createStyleSheet(() => ({
+        flexStyle: {
+            ...rest,
+        },
+    }));
+
+    const { styles } = useStyles(stylesheet);
+
+    const filterStyles = (styleObject: StyleObject): StyleObject => {
+        return Object.keys(styleObject)
+            .filter((key) => cssValue.includes(key))
+            .reduce((obj, key) => {
+                obj[key] = styleObject[key];
+                return obj;
+            }, {} as StyleObject);
+    };
+
+    return filterStyles(styles.flexStyle);
+};
+export default createAndFilterStyles;
