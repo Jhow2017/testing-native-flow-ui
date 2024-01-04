5040506b7e9c0cca8eb6b1f0a0d42f0fa4dcc5b7
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2024-01-04 01:39:04 -0400

    create hook useResolvedBreakpointValue

---
+++ src/designer-system/hooks/useResolvedBreakpointValue.tsx
@@ -0,0 +1,34 @@
+import { useMemo } from 'react';
+import { Dimensions } from 'react-native';
+import { BreakpointKeys, breakpoints } from '@ds/config/theme/breakpoints';
+
+export type BreakpointValue<T> = T | Partial<Record<BreakpointKeys, T>>;
+
+export default function useResolvedBreakpointValue<T>(
+    value: BreakpointValue<T>
+): T | undefined {
+    const windowWidth = Dimensions.get('window').width;
+
+    const getBreakpoint = (): BreakpointKeys => {
+        if (windowWidth >= breakpoints.tvLike) return 'tvLike';
+        if (windowWidth >= breakpoints.superLarge) return 'superLarge';
+        if (windowWidth >= breakpoints.xl) return 'xl';
+        if (windowWidth >= breakpoints.lg) return 'lg';
+        if (windowWidth >= breakpoints.md) return 'md';
+        if (windowWidth >= breakpoints.sm) return 'sm';
+        return 'xs';
+    };
+
+    const currentBreakpoint = getBreakpoint();
+
+    const resolvedValue = useMemo(() => {
+        if (typeof value === 'object' && value !== null) {
+            const breakpointValue = value as Partial<Record<BreakpointKeys, T>>;
+            return breakpointValue[currentBreakpoint] ?? breakpointValue.xs;
+        }
+
+        return value;
+    }, [value, currentBreakpoint]);
+
+    return resolvedValue;
+}
