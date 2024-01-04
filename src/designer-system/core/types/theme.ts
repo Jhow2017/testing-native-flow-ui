b466721138fe3990955ae04b9aec438cb045cf38
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2024-01-04 01:39:47 -0400

    remove type BreakpointKey in folder BaseThemeTypes

--- src/designer-system/core/types/theme.ts
+++ src/designer-system/core/types/theme.ts
@@ -8,7 +8,6 @@ export type FontFamily =
     | 'Inter_500Medium'
     | 'Inter_700Bold'
     | 'Inter_900Black';
-export type BreakpointKey = 'sm' | 'md' | 'lg';
 
 export type BaseThemeTypes = {
     backgroundColor?: BreakpointValue<Color>;
