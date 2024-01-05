6289bdfadac7239d24fe78133c064657b78a82e9
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2024-01-05 02:35:48 -0400

    add more tokens in FontSize

--- src/designer-system/config/theme/fontSize.ts
+++ src/designer-system/config/theme/fontSize.ts
@@ -1,8 +1,20 @@
 export const FontSize = {
-    xs: 12,
-    sm: 14,
-    md: 16,
-    lg: 18,
-    xl: 22,
-    xll: 24,
+    xs: 16,
+    sm: 18,
+    md: 20,
+    lg: 22,
+    xl: 24,
+    '2xl': 26,
+    '3xl': 28,
+    '4xl': 30,
+    '5xl': 32,
+    '6xl': 34,
+    '7xl': 36,
+    '8xl': 38,
+    '9xl': 40,
+    '10xl': 42,
+    '11xl': 44,
+    '12xl': 46,
 } as const;
+
+export type FontSizeType = keyof typeof FontSize;
