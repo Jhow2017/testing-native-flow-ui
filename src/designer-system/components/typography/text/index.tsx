6faee190eae6f76f35fbb567776ee5b7f2de6bc2
Jhow Gama <jonathan.nascimento.external@lojasrenner.com.br>
2024-01-06 02:22:00 -0400

    refactor DsText with ComponentMounter

--- src/designer-system/components/typography/text/index.tsx
+++ src/designer-system/components/typography/text/index.tsx
@@ -1,40 +1,35 @@
 import React from 'react';
-import { Platform } from 'react-native';
 
-// config
-import { createStyleSheet, useStyles } from '@ds/config/unistyles';
+import ComponentMounter from '@ds/core/component-mounter';
 
 // types
 import { DsTextTypes } from './types';
 
-// utils
-import { filterStyles } from '@ds/core/utils/filters-styles-or-props/filterStyles';
-
-// styles
-import { StyledText } from './styles';
-
 const DsText: React.FC<DsTextTypes> = (props) => {
-    const { children, numberOfLines, ellipsizeMode, _css, ...rest } = props;
+    const { children, variant, size, ...attr } = props;
 
-    const styleFilter = filterStyles(rest);
-    const stylesheet = createStyleSheet(() => ({
-        flexStyle: {
-            ...styleFilter,
-        },
-    }));
+    const fontSizeMapping = {
+        xxlarge: 42,
+        xlarge: 32,
+        large: 24,
+        medium: 16,
+        small: 12,
+    };
 
-    const { styles } = useStyles(stylesheet);
-    const platformStyles = rest?._platform ? rest?._platform(Platform) : {};
-    const additionalStyles: any =
-        rest.style instanceof Array ? rest.style : [rest.style];
+    const fontSize = fontSizeMapping[size || 'medium'] || 16;
 
     return (
-        <StyledText
-            {...props}
-            style={[styles.flexStyle, ...additionalStyles, platformStyles]}
+        <ComponentMounter
+            as="text"
+            {...attr}
+            style={attr?.style}
+            fontWeight={
+                attr?.fontWeight ?? variant === 'bold' ? 'bold' : 'normal'
+            }
+            fontSize={attr?.fontSize ?? fontSize}
         >
             {children}
-        </StyledText>
+        </ComponentMounter>
     );
 };
 
--- src/designer-system/components/typography/text/types.ts
+++ src/designer-system/components/typography/text/types.ts
@@ -1,16 +1,14 @@
 import type { ReactNode } from 'react';
-import { TextStyle, Platform } from 'react-native';
 
-import { BaseFlexTypes } from '@ds/core/types/flex';
-import { BaseTextTypesProps } from '@ds/core/types/text';
-import { BaseThemeTypes } from '@ds/core/types/theme';
-import { BreakpointValue } from '@ds/core/types/breakpointValue';
+// types
+import type { BaseFlexTypes } from '@ds/core/types/flex';
+import type { BaseTextTypesProps } from '@ds/core/types/text';
+import type { BaseThemeTypes } from '@ds/core/types/theme';
 
 export type DsTextTypes = BaseFlexTypes &
     BaseTextTypesProps &
     BaseThemeTypes & {
         children?: ReactNode;
-        as?: 'p' | 'small' | 'span' | 'strong' | 'b';
         variant?: 'normal' | 'bold';
-        size?: 'xxlarge' | 'xlarge' | 'large';
+        size?: 'xxlarge' | 'xlarge' | 'large' | 'medium' | 'small';
     };
